4017fb93b453afd6c53f0f8bd05c7d02258489e8
Anatolii Gorbenko <dragondrake1994@gmail.com>
2024-04-23 20:34:30 +0700

    Implemented copying of verb forms to the clipboard and added console input for verb

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,30 @@
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         ReversoParser parser = new ReversoParser();
-        parser.parseForms("devoir");
-        System.out.println(parser);
+        Scanner scanner = new Scanner(System.in);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        StringSelection selection;
+        String verb = "";
+        while (!verb.equals("0")) {
+            System.out.print("Entrez le verbe: ");
+            verb = scanner.nextLine();
+            if (verb.equals("0")) {
+                continue;
+            }
+            String forms = parser.getForms(verb);
+
+            if (!forms.isEmpty()) {
+                selection = new StringSelection(forms);
+                clipboard.setContents(selection, null);
+            }
+        }
+        scanner.close();
+
     }
 }
--- src/main/java/ReversoParser.java
+++ src/main/java/ReversoParser.java
@@ -1,55 +1,60 @@
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class ReversoParser {
-    private final ArrayList<String> formes;
-    private final String LINK = "https://conjugueur.reverso.net/conjugaison-francais-verbe-";
+    public static final String INDICATIF_PRESENT_CSS_QUERRY = "#ch_divSimple .blue-box-wrap[mobile-title=\"Indicatif Présent\"] li";
+    public static final String PARTICIP_PASSE_CSS_QUERRY = "#ch_divSimple .blue-box-wrap[mobile-title=\"Participe Passé\"] li i[class=\"verbtxt\"]";
+    private final String URL = "https://conjugueur.reverso.net/conjugaison-francais-verbe-";
 
-    public ReversoParser() {
-        this.formes = new ArrayList<>();
-    }
-
-    public void parseForms(String verb) throws IOException {
+    private StringBuilder parseIndicatifPresent(Document doc) {
         String line;
-        Document doc = Jsoup.connect(LINK + verb + ".html").get();
-        Elements formes = doc.select("#ch_divSimple .blue-box-wrap[mobile-title=\"Indicatif Présent\"] li");
+        StringBuilder builder = new StringBuilder();
+        Elements formes = doc.select(INDICATIF_PRESENT_CSS_QUERRY);
         for (Element f : formes) {
             line = f.text();
             if (line.contains("/")) {
                 line = line.substring(0, line.indexOf('/')) + line.substring(line.indexOf(" "));
             }
-            this.formes.add(line);
+            builder.append(line)
+                    .append(";\n");
         }
-        Elements participePasse = doc.select("#ch_divSimple .blue-box-wrap[mobile-title=\"Participe Passé\"] li i[class=\"verbtxt\"]");
+        return builder;
+    }
+
+    private StringBuilder parseParticipePasse(Document doc) {
+        StringBuilder builder = new StringBuilder();
+        Elements participePasse = doc.select(PARTICIP_PASSE_CSS_QUERRY);
         if (participePasse.size() == 4) {
             String mascSg = participePasse.get(0).text();
             String mascPl = participePasse.get(1).text();
             boolean isRegularForm = mascPl.startsWith(mascSg);
             String termination = "(e)(s)";
             if (isRegularForm) {
-                this.formes.add(mascSg + termination);
+                builder.append(mascSg).append(termination);
             } else {
-                this.formes.add(mascSg + "," + mascPl.substring(0, mascPl.length() - 1) + termination);
+                builder.append(mascSg).append(",").append(mascPl, 0, mascPl.length() - 1).append(termination);
             }
         }
+        return builder;
     }
 
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        Iterator<String> iterator = formes.iterator();
-        while (iterator.hasNext()) {
-            builder.append(iterator.next());
-            if (iterator.hasNext()) {
-                builder.append(";")
-                        .append("\n");
-            }
+    public String getForms(String verb) throws IOException {
+        Document doc;
+        try {
+            doc = Jsoup.connect(URL + verb + ".html").get();
+        } catch (HttpStatusException e) {
+            System.out.println("Il n'y a pas ce verbe: " + verb);
+            return "";
         }
-        return builder.toString();
+
+        StringBuilder indicatifPresent = parseIndicatifPresent(doc);
+        StringBuilder participePasse = parseParticipePasse(doc);
+
+        return indicatifPresent.append(participePasse).toString();
     }
 }
